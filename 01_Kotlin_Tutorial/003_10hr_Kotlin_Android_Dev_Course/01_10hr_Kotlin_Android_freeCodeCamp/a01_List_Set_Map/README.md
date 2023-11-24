0b0382482b590eb95e2655d7de48b4d751bc6fb7
GYoung <ytok1108@gmail.com>
2023-11-24 11:46:44 +0900

    remove

--- 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a01_List_Set_Map/README.md
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a01_List_Set_Map/README.md
@@ -27,3 +27,16 @@ Name 3
 Name 4  
   
 ```
+
+
+- remove
+
+```
+$ kotlinc src/Main.kt -include-runtime -d out/Main.jar && java -jar out/Main.jar
+src\Main.kt:1:10: warning: parameter 'args' is never used
+fun main(args: Array<String>) {
+         ^
+Name 3
+Name 4  
+  
+```
--- 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a01_List_Set_Map/src/Main.kt
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a01_List_Set_Map/src/Main.kt
@@ -2,8 +2,10 @@ fun main(args: Array<String>) {
     val names = mutableListOf("Name 1", "Name 2", "Name 3")
 
     names.add("Name 4")
-    // remove
+    // removeAt
     names.removeAt(0)
+    // remove "name 2"
+    names.remove("Name 2")
 
     names.forEach { println(it)}
 }
