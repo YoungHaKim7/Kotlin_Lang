34d2c7c342006fba236043e423eca501eb36d48f
YoungHa Kim <ytok1108@aol.com>
2023-11-26 10:34:01 +0900

    for 문 자바랑 동일

--- 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a10_for_foreach/src/Main.kt
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a10_for_foreach/src/Main.kt
@@ -1,8 +1,11 @@
 fun main() {
   val items = listOf(1,2,3,4,5)
 
-  for (item in items) {
-    print(item)
-  }
+  // for (item in items) {
+  //   print(item)
+  // }
+  items.forEach { item ->
+    print(item)}
+
   print('\n')
 }
