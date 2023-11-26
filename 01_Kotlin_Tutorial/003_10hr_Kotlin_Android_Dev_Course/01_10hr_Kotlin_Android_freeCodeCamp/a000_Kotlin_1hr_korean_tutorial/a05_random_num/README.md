5f366b0c74c916170d1d35ca8eca25c5e082c100
YoungHa Kim <ytok1108@aol.com>
2023-11-26 14:06:50 +0900

    Math __random number

---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a05_random_num/README.md
@@ -0,0 +1,13 @@
+# Result
+
+- ```var randomNumber = Random.nextInt()```
+
+
+```
+1181913666
+```
+
+- ```var randomNumber = Random.nextInt(0, 100)```
+```
+70  
+```
---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a05_random_num/src/Main.kt
@@ -0,0 +1,10 @@
+import kotlin.random.Random
+
+fun main() {
+  // var randomNumber = Random.nextInt()
+  var randomNumber = Random.nextInt(0, 100)
+  print(randomNumber)
+  print('\n')
+}
+
+
