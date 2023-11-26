7eb0ab840daba1e82c89b1251029593288c3c4d5
YoungHa Kim <ytok1108@aol.com>
2023-11-26 11:00:09 +0900

    mutableListOf

---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a14_mutableListof/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+  // val items : MutableList<Int> = mutableListOf(1,2,3,4,5)  생략된 형태 
+  val items = mutableListOf(1,2,3,4,5)
+
+  items.add(6)
+  items.remove(3)
+  println(items)
+}
+
+
