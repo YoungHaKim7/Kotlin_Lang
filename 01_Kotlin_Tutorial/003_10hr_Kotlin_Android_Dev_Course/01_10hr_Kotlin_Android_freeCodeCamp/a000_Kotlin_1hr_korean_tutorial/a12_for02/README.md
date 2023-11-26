69466f72434565ef2f7d948e2e3c44cd4c5fb6c8
YoungHa Kim <ytok1108@aol.com>
2023-11-26 10:54:32 +0900

    for 02

---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a12_for02/README.md
@@ -0,0 +1,11 @@
+# Result
+
+
+```
+
+$ kotlinc ./src/Main.kt  -include-runtime -d ./out/Main.jar
+$ java -jar ./out/Main.jar
+
+0123
+
+```
---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a12_for02/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+  val items = listOf(1,2,3,4,5)
+
+  //(java) for (int i = 0; i < items.lenth; i++)
+  for (i in 0..3) {
+    print(i)
+  }
+  print('\n')
+}
+
+
