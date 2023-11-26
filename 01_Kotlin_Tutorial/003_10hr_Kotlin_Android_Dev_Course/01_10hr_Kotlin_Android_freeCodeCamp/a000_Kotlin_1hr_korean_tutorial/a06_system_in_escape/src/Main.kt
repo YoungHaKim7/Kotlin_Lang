bbeab7f8183ecfbc6c886a678d67bbb111aa398b
YoungHa Kim <ytok1108@aol.com>
2023-11-26 14:14:16 +0900

    input

---
+++ 01_Kotlin_Tutorial/003_10hr_Kotlin_Android_Dev_Course/01_10hr_Kotlin_Android_freeCodeCamp/a000_Kotlin_1hr_korean_tutorial/a06_system_in_escape/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    // 숫자로 받기 
+    // var input = reader.nextInt()
+
+    // String으로 받기
+    var input = reader.next()
+    println("Your input: $input")
+}
+
